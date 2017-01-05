4ab307c60421cbbda4694129666c0b0ec7f7a8e9
Shukan Shah <shukanshah14@gmail.com>
2017-01-04 22:53:29 -0500

    Working with notifications and alarm manager in Android

---
+++ app/src/main/java/com/example/shuka/notificationexample/AlertReceiver.java
@@ -0,0 +1,58 @@
+package com.example.shuka.notificationexample;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
+
+/**
+ * Created by shuka on 1/3/2017.
+ */
+
+public class AlertReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        createNotification(context, "Times Up", "5 Seconds Had Passed", "Alert");
+
+    }
+
+    public void createNotification(Context context, String msg, String msgText, String msgAlert){
+
+        // Define an Intent and an action to perform with it by another application
+        PendingIntent notificIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), 0);
+
+        // Builds a notification
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.icon)
+                        .setContentTitle(msg)
+                        .setTicker(msgAlert)
+                        .setContentText(msgText);
+
+        // Defines the Intent to fire when the notification is clicked
+        mBuilder.setContentIntent(notificIntent);
+
+        // Set the default notification option
+        // DEFAULT_SOUND : Make sound
+        // DEFAULT_VIBRATE : Vibrate
+        // DEFAULT_LIGHTS : Use the default light notification
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+
+        // Auto cancels the notification when clicked on in the task bar
+        mBuilder.setAutoCancel(true);
+
+        // Gets a NotificationManager which is used to notify the user of the background event
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Post the notification
+        mNotificationManager.notify(1, mBuilder.build());
+
+    }
+}
---
+++ app/src/main/java/com/example/shuka/notificationexample/MoreInfoNotification.java
@@ -0,0 +1,16 @@
+package com.example.shuka.notificationexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shuka on 1/3/2017.
+ */
+
+public class MoreInfoNotification extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_info_notific);
+    }
+}
